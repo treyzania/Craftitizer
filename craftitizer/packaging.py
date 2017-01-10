ef2ea5a0a9a271fb8611976b73d500ec0fa4b9ce
treyzania <treyzania@gmail.com>
2017-01-10 16:16:20 -0500

    Added more information classes for the packaging module.

--- craftitizer/packaging.py
+++ craftitizer/packaging.py
@@ -35,8 +35,28 @@ class Package:
 	def state_prestop(self, ctx):
 		pass
 
+class PackageMeta:
+	def __init__(self, name, version):
+		self.name = name
+		self.version = version
+
 class Context:
 		
 	def __init__(self, name):
 		self.pkgname = name
+
+class LoadContext(Context):
 	
+	def __init__(self, name, ldr):
+		super(name)
+		self.loader = ldr
+		self.deps = []
+		self.repos = []
+	
+	def depend(name, version):
+		self.deps.append(PackageMeta(name, version))
+	
+	def specify_repo(name, url):
+		self.repos.append({'name': name, 'url': url})
+
+
