3d0071d02008017ef570d3806abbada813dc4de9
treyzania <treyzania@gmail.com>
2017-02-14 14:10:22 -0500

    Mostly got crsplit dir creation working.

--- crsplit/crsplit.c
+++ crsplit/crsplit.c
@@ -15,6 +15,29 @@
 #define WORK_DIR_FLAGS (S_IRWXU)
 #define LIVE_DIR_FLAGS (S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
 
+char* mallocconcat(char* a, char* b) {
+
+	int len = strlen(a) + strlen(b) + 1;
+	char* out = malloc(len);
+
+	strcat(out, a);
+	strcat(out, b);
+
+	return out;
+
+}
+
+void makeextdir(char* base, char* ext, int flags) {
+
+	char* fullname = mallocconcat(base, ext);
+	if (mkdir(fullname, flags) != 0) {
+		printf("Could not make %s dir.", ext);
+	}
+
+	free(fullname);
+
+}
+
 int main(int argc, char* argv[]) {
 
 	if (argc != 2) {
@@ -30,34 +53,9 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	char edp[100], ddp[100], wdp[100], ldp[100];
-	strcpy(edp, splitpath);
-	strcat(edp, EXEC_DIR_NAME);
-	strcpy(ddp, splitpath);
-	strcat(edp, DATA_DIR_NAME);
-	strcpy(wdp, splitpath);
-	strcat(wdp, WORK_DIR_NAME);
-	strcpy(ldp, splitpath);
-	strcat(ldp, LIVE_DIR_NAME);
-
-	if (mkdir(edp, EXEC_DIR_FLAGS) != 0) {
-		printf("Could not make exec dir.");
-		return -1;
-	}
-
-	if (mkdir(ddp, DATA_DIR_FLAGS) != 0) {
-		printf("Could not make exec dir.");
-		return -1;
-	}
-
-	if (mkdir(wdp, WORK_DIR_FLAGS) != 0) {
-		printf("Could not make exec dir.");
-		return -1;
-	}
-
-	if (mkdir(ldp, LIVE_DIR_FLAGS) != 0) {
-		printf("Could not make exec dir.");
-		return -1;
-	}
+	makeextdir(splitpath, EXEC_DIR_NAME, EXEC_DIR_FLAGS);
+	makeextdir(splitpath, DATA_DIR_NAME, DATA_DIR_FLAGS);
+	makeextdir(splitpath, WORK_DIR_NAME, WORK_DIR_FLAGS);
+	makeextdir(splitpath, LIVE_DIR_NAME, LIVE_DIR_FLAGS);
 
 }
