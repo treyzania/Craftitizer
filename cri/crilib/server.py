6a0217129215e523584b96b3a94230ca8831c0eb
treyzania <treyzania@gmail.com>
2017-02-23 00:50:51 -0500

    Added some stuff to get the non-forking Java launcher working.

--- cri/crilib/server.py
+++ cri/crilib/server.py
@@ -1,6 +1,7 @@
 import os
-import ruamel.yaml
+import os.path
 import subprocess
+import ruamel.yaml
 
 class Server(object):
     def __init__(self, path):
@@ -30,8 +31,44 @@ class DebugExecutor(Executor):
 class SimpleJavaExecutor(Executor):
     def __init__(self, serv, params):
         super().__init__(serv)
+
+        self.jxms = params["start_mem"]
+        self.jxmx = params["max_mem"]
+        self.jar = params["jar"]
+        self.extras = [] # TODO Implement this.
+
+        if "java_cmd" in params.values():
+            self.java = params["java_cmd"]
+        else:
+            self.java = "java"
+
+        if "subdir" in params.values():
+            self.subdir = params["subdir"]
+        else:
+            self.subdir = "server"
+
     def start(self):
-        pass
+        try:
+            os.stat(self.subdir)
+        except IOError:
+            os.mkdir(self.subdir)
+
+        if not os.path.isfile(os.path.join(self.subdir, self.jar)):
+            raise Exception("Server jar does not exist.")
+
+        abspath = os.path.abspath(self.subdir)
+        command = []
+        command.append("-Xms" + self.jxms + "M")
+        command.append("-Xmx" + self.jxmx + "M")
+        command.append("-jar")
+        command.append(self.jar)
+        command.append("nogui")
+
+         # TODO Take env=[...] parameters.
+        with subprocess.Popen(command, executable=self.java, cwd=abspath) as proc:
+            ret = proc.wait()
+            if ret != 0:
+                print("Server exited with non-zero return code " + str(ret))
 
 executors = {
     'test': DebugExecutor,
