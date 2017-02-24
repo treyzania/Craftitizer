b1a1a12536013a27f0ef9f1dfd1e418efb661700
treyzania <treyzania@gmail.com>
2017-02-24 00:08:53 -0500

    Cleaned up the packaing class again.

--- cri/crilib/packaging.py
+++ cri/crilib/packaging.py
@@ -1,38 +1,27 @@
-import craftitizer.repositories
+import crilib.repositories
 
 class Package:
-	
 	def stage_load(self, ctx):
 		print(ctx.pkgname)
 		pass
-	
 	def stage_install(self, ctx):
 		pass
-	
 	def stage_configure(self, ctx):
 		pass
-	
 	def stage_postconf(self, ctx):
 		pass
-	
 	def stage_start(self, ctx):
 		pass
-	
 	def stage_poststart(self, ctx):
 		pass
-	
 	def stage_crash(self, ctx):
 		pass
-	
 	def stage_exit(self, ctx):
 		pass
-	
 	def stage_preupgrade(self, ctx):
 		pass
-	
 	def stage_postupgrade(self, ctx):
 		pass
-	
 	def state_prestop(self, ctx):
 		pass
 
@@ -42,22 +31,20 @@ class PackageMeta:
 		self.version = version
 
 class Context:
-	
+
 	def __init__(self, name):
 		self.pkgname = name
 
 class LoadContext(Context):
-	
+
 	def __init__(self, name, ldr):
 		super(name)
 		self.loader = ldr
 		self.deps = []
 		self.repos = []
-	
+
 	def depend(name, version):
 		self.deps.append(PackageMeta(name, version))
-	
+
 	def specify_repo(name, url):
 		self.repos.append(RepoMeta(name, url))
-
-
