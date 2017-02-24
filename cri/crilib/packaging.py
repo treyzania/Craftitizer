2da423dd246a4e2d68f3d0df722403babceeebe9
treyzania <treyzania@gmail.com>
2017-02-24 01:06:06 -0500

    Removed confloader module and prepared packaging module for further work.

--- cri/crilib/packaging.py
+++ cri/crilib/packaging.py
@@ -1,44 +1,26 @@
 import crilib.repositories
 
-class Package:
-	def stage_load(self, ctx):
-		print(ctx.pkgname)
-		pass
-	def stage_install(self, ctx):
-		pass
-	def stage_configure(self, ctx):
-		pass
-	def stage_postconf(self, ctx):
-		pass
-	def stage_start(self, ctx):
-		pass
-	def stage_poststart(self, ctx):
-		pass
-	def stage_crash(self, ctx):
-		pass
-	def stage_exit(self, ctx):
-		pass
-	def stage_preupgrade(self, ctx):
-		pass
-	def stage_postupgrade(self, ctx):
-		pass
-	def state_prestop(self, ctx):
-		pass
+# Stages:
+# * load
+# * install
+# * configure
+# * postconf
+# * start
+# * poststart
+# * crash
+# * exists
+# * preupgrade
+# * postupgrade
+# * prestop
 
 class Context:
 	def __init__(self, name):
 		self.pkgname = name
 
 class LoadContext(Context):
-
 	def __init__(self, name, ldr):
 		super(name)
 		self.loader = ldr
 		self.deps = []
-		self.repos = []
-
 	def depend(name, version):
 		self.deps.append(PackageMeta(name, version))
-
-	def specify_repo(name, url):
-		self.repos.append(RepoMeta(name, url))
